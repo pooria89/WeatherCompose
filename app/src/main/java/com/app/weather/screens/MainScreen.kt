18879bca9c5cfa6c313b27fc8fcfa0fcb26e220a
pooria89 <pooria.maleki89@gmail.com>
2022-09-07 16:33:05 +0430

    add navgraph

---
+++ app/src/main/java/com/app/weather/screens/MainScreen.kt
@@ -0,0 +1,17 @@
+package com.app.weather.screens
+
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import com.ramcosta.composedestinations.annotation.Destination
+import com.ramcosta.composedestinations.navigation.DestinationsNavigator
+
+@Destination
+@Composable
+fun MainScreen(navigator: DestinationsNavigator) {
+    Button(onClick = {
+
+    }) {
+
+    }
+}
---
+++ app/src/main/java/com/app/weather/screens/SplashScreen.kt
@@ -0,0 +1,40 @@
+package com.app.weather.screens
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.animateLottieCompositionAsState
+import com.airbnb.lottie.compose.rememberLottieComposition
+import com.app.weather.R
+import com.ramcosta.composedestinations.annotation.Destination
+import com.ramcosta.composedestinations.annotation.RootNavGraph
+import com.ramcosta.composedestinations.navigation.DestinationsNavigator
+
+@RootNavGraph(start = true)
+@Destination
+@Composable
+fun SplashScreen(navigator: DestinationsNavigator) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colors.primary)
+    ) {
+        val composition by rememberLottieComposition(LottieCompositionSpec.RawRes(R.raw.splash_day))
+        val logoAnimationState =
+            animateLottieCompositionAsState(composition = composition)
+        LottieAnimation(
+            composition = composition,
+            progress = { logoAnimationState.progress }
+        )
+//        Button(onClick = { navigator.navigate() }) {
+//
+//        }
+    }
+}
