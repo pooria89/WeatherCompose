eeb8b1b95c7ff04ae94ada3a1de54d8e8ea0667e
pooria89 <pooria.maleki89@gmail.com>
2022-09-10 10:25:12 +0430

    add navGraph to project .

--- app/src/main/java/com/app/common/App.kt
+++ app/src/main/java/com/app/common/App.kt
@@ -1,11 +1,12 @@
 package com.app.common
 
 import android.app.Application
+import android.content.Context
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class App : Application() {
-    override fun onCreate() {
-        super.onCreate()
+    override fun getApplicationContext(): Context {
+        return super.getApplicationContext()
     }
 }
--- app/src/main/java/com/app/weather/MainActivity.kt
+++ app/src/main/java/com/app/weather/MainActivity.kt
@@ -3,15 +3,23 @@ package com.app.weather
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import com.app.weather.screens.NavGraphs
 import com.ramcosta.composedestinations.DestinationsNavHost
+import com.ramcosta.composedestinations.rememberNavHostEngine
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            val engine = rememberNavHostEngine()
+            val navController = engine.rememberNavController()
 
             DestinationsNavHost(
-                navGraph = NavGraphs.root
+                navGraph = NavGraphs.root,
+                navController = navController,
+                engine = engine
             )
 
         }
